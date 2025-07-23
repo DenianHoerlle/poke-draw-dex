88b6c26570f4b083786ab1fb90e6b9e01074352a
DenianHoerlle <deniancloss@gmail.com>
2025-07-23 13:52:04 -0300

    chore: :wrench: added prettier config file

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    plugins: ['prettier-plugin-tailwindcss'],
+    tailwindStylesheet: './src/app/globals.css',
+}
+
+export default config
