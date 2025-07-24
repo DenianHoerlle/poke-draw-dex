f27dc1866d4c1fbd29ef689ff659b730bf1bafcb
DenianHoerlle <deniancloss@gmail.com>
2025-07-23 22:48:45 -0300

    feat: :sparkles: added header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,20 @@
+import { Images } from "@/assets"
+import Image from "next/image"
+import Link from "next/link"
+
+export function Header() {
+  return (
+    <header className="relative flex items-center border-b border-black px-4">
+      <Link
+        href="/home"
+        className="absolute left-1/2 flex translate-x-[50%] gap-2"
+      >
+        <Image width={20} height={20} src={Images.logo} alt="poke-logo" />
+        PokéDrawDex!
+      </Link>
+      <Link href="/dex" className="px-1 py-2 uppercase underline">
+        Dex
+      </Link>
+    </header>
+  )
+}
